d7d657cf41e6c56ccb853bf0bf356a17a5a0921a
YangyangLI <li002252@umn.edu>
2020-12-21 22:41:47 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='build one classifier for lncRNA analysis ',
+    author='YangyangLi ',
+    license='MIT',
+)
