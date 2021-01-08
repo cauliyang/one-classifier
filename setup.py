40a738c366e05f8868bb054815d26ad0f4eaf1d1
YangyangLI <li002252@umn.edu>
2021-01-08 18:57:33 +0800

    add flake8 and pre-commit

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import find_packages, setup
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
 # test
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='build one classifier for lncRNA analysis ',
-    author='YangyangLi ',
-    license='MIT',
+    version="0.1.0",
+    description="build one classifier for lncRNA analysis ",
+    author="YangyangLi ",
+    license="MIT",
 )
